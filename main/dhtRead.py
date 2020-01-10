8bb24f73c8254de23b8d9b217485474d571fe41f
sanal <sanal.madhavan@gmail.com>
2020-01-10 12:26:18 +0000

    refactor

---
+++ main/dhtRead.py
@@ -0,0 +1,32 @@
+from time import sleep
+from machine import Pin
+from dht import DHT22
+import ujson
+import machine
+
+sensor = DHT22(Pin(15, Pin.IN, Pin.PULL_UP))   # DHT-22 on GPIO 15 (input with internal pull-up resistor)
+blueLed = machine.Pin(2, machine.Pin.OUT)
+ledInterval = 0.01
+readingInterval = 1
+
+while True:
+    try:
+        sensor.measure()   # Poll sensor
+        t = sensor.temperature()
+        h = sensor.humidity()
+        if isinstance(t, float) and isinstance(h, float):  # Confirm sensor results are numeric
+            reading = {}
+            reading["temperature"] = t
+            reading["humidity"] = h
+            reading["interval"] = readingInterval
+            encodedReading = ujson.dumps(reading)
+            print(encodedReading)
+        else:
+            print('Invalid sensor readings.')
+    except OSError:
+        print('Failed to read sensor.')
+    blueLed.value(1)
+    sleep(ledInterval)
+    blueLed.value(0)
+    sleep(ledInterval)
+    sleep(readingInterval)
