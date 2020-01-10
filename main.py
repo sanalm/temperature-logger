036c7eb9125209bd6f908fa211f101bd525ec989
sanal <sanal.madhavan@gmail.com>
2020-01-10 12:24:32 +0000

    refactor

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-# from ota_update.main.ota_updater import OTAUpdater
-from ota_updater import OTAUpdater
+from main.config import Config
+from main.ota_updater import OTAUpdater
 
 def download_and_install_update_if_available():
-    o = OTAUpdater('https://github.com/sanalm/temperature-logger.git')
-    o.download_and_install_update_if_available('wifi-ssid', 'wifi-password')
+    o = OTAUpdater('https://github.com/sanalm/temperature-logger')
+    o.using_network(ssid, password)
+    o.check_for_update_to_install_during_next_reboot()
+    # o.download_and_install_update_if_available('VM4420671', 'k5qdPdscrybp')
+    # o.download_updates_if_available()
+    # print(o.get_latest_version())
 
 def start():
     # your custom code goes here. Something like this: ...
     # from main.x import YourProject
     # project = YourProject()
     # ...
-    import dhtRead
+    from main.dhtRead import dhtRead
 
 
 def boot():
     download_and_install_update_if_available()
     start()
+
+# load configuration for a file
+config = Config('main.conf')
+ssid = config.get('ssid')
+password = config.get('password')
 
 boot()
